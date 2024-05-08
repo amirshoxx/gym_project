fcf5eeba612240d208f2a248ed23cd80616fb129
Asus vivobook <sardorsanoyev441@gmail.com>
2024-05-08 16:32:31 +0500

    UPDATE AddAdmin.jsx

--- gym-frontend/src/AddAdmin.jsx
+++ gym-frontend/src/AddAdmin.jsx
@@ -1,12 +1,22 @@
+import {useState} from "react";
+import apiCall from "./apicall/apiCall.js";
 
 function AddAdmin() {
+    const [fullName, setFullName] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+    const [password, setPassword] = useState("")
+
+    function addAdmin() {
+        apiCall("/admin", "POST",{fullName, phoneNumber, password})
+    }
+
     return (
         <div>
             <div>
-                <input type={"text"} placeholder={"Ism..."}/>
-                <input type={"text"} placeholder={"Telefon raqam..."}/>
-                <input type={"text"} placeholder={"Parol..."}/>
-                <button>{"Qo'shish"}</button>
+                <input type={"text"} value={fullName} onChange={(e)=>setFullName(e.target.value)} placeholder={"Ism..."}/>
+                <input type={"text"} value={phoneNumber} onChange={(e)=>setPhoneNumber(e.target.value)} placeholder={"Telefon raqam..."}/>
+                <input type={"text"} value={password} onChange={(e)=>setPassword(e.target.value)} placeholder={"Parol..."}/>
+                <button onClick={()=>addAdmin()}>{"Qo'shish"}</button>
             </div>
         </div>
     );
