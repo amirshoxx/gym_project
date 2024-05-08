06c51fced564bfe0f7092a0efcd4ae9ff35fcedf
Asus vivobook <sardorsanoyev441@gmail.com>
2024-05-08 15:35:28 +0500

    update AdminController

--- gym-backend/src/main/java/org/example/gymbackend/controller/AdminController.java
+++ gym-backend/src/main/java/org/example/gymbackend/controller/AdminController.java
@@ -5,6 +5,7 @@ import org.example.gymbackend.dto.AdminDto;
 import org.example.gymbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
     @Autowired
     UserService userService;
-
+    @PreAuthorize("hasAnyRole('ROLE_SUPER_ADMIN')")
     @PostMapping
     public ResponseEntity<?> saveAdmin(@RequestBody @Valid AdminDto dto) {
         return userService.saveAdmin(dto);
