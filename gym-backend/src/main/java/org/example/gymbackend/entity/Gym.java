5c0a235852441d3ef0f625a36a7d902eff47be66
Asus vivobook <sardorsanoyev441@gmail.com>
2024-05-08 14:30:50 +0500

    update application.properties

--- gym-backend/src/main/java/org/example/gymbackend/entity/Gym.java
+++ gym-backend/src/main/java/org/example/gymbackend/entity/Gym.java
@@ -1,14 +1,12 @@
 package org.example.gymbackend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -22,4 +20,6 @@ public class Gym {
     private UUID id;
     private String name;
     private String location;
+    @OneToMany
+    private List<User> admins;
 }
