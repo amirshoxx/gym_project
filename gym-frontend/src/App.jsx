6a4f4c19eb7187c424f6c10021142b385eae3882
abdugafforov ozodbek <abdugafforovozodbek86@gmail.com>
2024-05-10 10:07:42 +0500

    add security

--- gym-frontend/src/App.jsx
+++ gym-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import "react"
 import {Route, Routes} from "react-router-dom";
 import Login from "./auth/Login.jsx";
 import Home from "./home/Home.jsx";
-import SuperAdmin from "./auth/Super_Admin.jsx";
+// import SuperAdmin from "./auth/Super_Admin.jsx";
 import Admin from "./auth/Admin.jsx";
 function App() {
 
@@ -10,7 +10,7 @@ function App() {
         <Routes>
           <Route path={"/login"} element={<Login/>}/>
             <Route path={"/"} element={<Home />} />
-            <Route path={"/super_admin_page"} element={<SuperAdmin />} />
+            {/*<Route path={"/super_admin_page"} element={<SuperAdmin />} />*/}
             <Route path={"/admin_page"} element={<Admin />} />
           {/*<Route path={"/register"} element={<Regis />} />*/}
         </Routes>
--- gym-frontend/src/auth/Super_Admin.jsx
+++ gym-frontend/src/auth/Super_Admin.jsx
@@ -1,32 +1,32 @@
-import React, {useEffect} from 'react';
-import {useNavigate} from "react-router-dom";
-import apiCall from "../apicall/apiCall.js";
-
-function SuperAdmin() {
-
-    const naviget = useNavigate();
-
-    useEffect(()=>{
-
-
-        if (localStorage.getItem("access_token")!=null){
-            apiCall(`/user/super_admin`, "GET",{}, { Authorization:localStorage.getItem("access_token") })
-                .then(() => {
-                })
-                .catch(() => {
-                    naviget("/admin_page");
-                });
-
-        }else {
-            naviget("/404")
-        }
-
-    },[])
-    return (
-        <div>
-            salom
-        </div>
-    );
-}
-
-export default SuperAdmin;
+// import React, {useEffect} from 'react';
+// import {useNavigate} from "react-router-dom";
+// import apiCall from "../apicall/apiCall.js";
+//
+// function SuperAdmin() {
+//
+//     const naviget = useNavigate();
+//
+//     useEffect(()=>{
+//
+//
+//         if (localStorage.getItem("access_token")!=null){
+//             apiCall(`/user/super_admin`, "GET",{}, { Authorization:localStorage.getItem("access_token") })
+//                 .then(() => {
+//                 })
+//                 .catch(() => {
+//                     naviget("/admin_page");
+//                 });
+//
+//         }else {
+//             naviget("/404")
+//         }
+//
+//     },[])
+//     return (
+//         <div>
+//             salom
+//         </div>
+//     );
+// }
+//
+// export default SuperAdmin;
